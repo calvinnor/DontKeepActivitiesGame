c56fcc5531be4b666524478539e5a1ab498886d9
Calvin Noronha <calvin.nrnha@gmail.com>
2019-10-02 12:35:07 +0530

    :hammer: Refactor to use Kotlin @Parcelize

--- app/src/main/java/com/calvinnor/dontkeepactivitiesgame/data/GameState.kt
+++ app/src/main/java/com/calvinnor/dontkeepactivitiesgame/data/GameState.kt
@@ -1,40 +1,12 @@
 package com.calvinnor.dontkeepactivitiesgame.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class GameState(
     val playerOneDetails: PlayerDetails,
     val playerTwoDetails: PlayerDetails,
     var playerOneScore: Long,
     var playerTwoScore: Long
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        playerOneDetails = parcel.readParcelable(PlayerDetails::class.java.classLoader),
-        playerTwoDetails = parcel.readParcelable(PlayerDetails::class.java.classLoader),
-        playerOneScore = parcel.readLong(),
-        playerTwoScore = parcel.readLong()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeParcelable(playerOneDetails, flags)
-        parcel.writeParcelable(playerTwoDetails, flags)
-        parcel.writeLong(playerOneScore)
-        parcel.writeLong(playerTwoScore)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<GameState> {
-        override fun createFromParcel(parcel: Parcel): GameState {
-            return GameState(parcel)
-        }
-
-        override fun newArray(size: Int): Array<GameState?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
--- app/src/main/java/com/calvinnor/dontkeepactivitiesgame/data/PlayerDetails.kt
+++ app/src/main/java/com/calvinnor/dontkeepactivitiesgame/data/PlayerDetails.kt
@@ -1,35 +1,10 @@
 package com.calvinnor.dontkeepactivitiesgame.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class PlayerDetails(
     val id: Int,
     val name: String
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        id = parcel.readInt(),
-        name = parcel.readString() as String
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(name)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<PlayerDetails> {
-
-        override fun createFromParcel(parcel: Parcel): PlayerDetails {
-            return PlayerDetails(parcel)
-        }
-
-        override fun newArray(size: Int): Array<PlayerDetails?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
